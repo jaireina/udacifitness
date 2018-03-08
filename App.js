34e3ce7579e4b1aa161a55352d5f9e9d32770074
Jair Reina <Jair.Reina@digitaslbi.com>
2018-03-07 22:03:27 -0600

    adding calendar, tabs and status bar

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ import {
   TouchableWithoutFeedback,
   Slider,
   Image,
-  Picker
+  Picker,
+  Platform,
+  PlatformIOS,
+  StatusBar
 } from 'react-native';
 import AddEntry from './components/AddEntry';
 import ListTest from './components/ListTest';
@@ -18,7 +21,54 @@ import Flex from './components/Flex';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
+import History from './components/History';
+import {TabNavigator, StackNavigator, DrawerNavigator} from 'react-navigation';
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+import {purple, white} from './utils/colors';
+import {Constants} from 'expo';
 
+function UdaciStatusBar({backgroundColor, ...props}){
+  return(
+    <View style={{backgroundColor, height:Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  );
+}
+
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions:{
+      tabBarLabel: 'History',
+      tabBarIcon: ({tintColor})=> <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  Dashboard: {
+    screen: AddEntry,
+    navigationOptions:{
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({tintColor})=> <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+},{ 
+  // navigationOptions:{
+  //   header: null
+  // }, 
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple:white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white: purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
 
 export default class App extends Component {
   // state = {
@@ -76,19 +126,24 @@ export default class App extends Component {
             source={{uri:'https://orig00.deviantart.net/1466/f/2017/185/b/f/wonder_woman___gal_gadot_by_raffaelemarinetti-dbf2jjx.jpg'}} 
           /> */}
 
-          <AddEntry />
-          
+          {/* <AddEntry /> */}
+
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
+            {/* <History /> */}
+          <Tabs />
         </View>
       </Provider>
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginLeft: 10,
-    marginRight: 10, 
+    // marginLeft: 10,
+    // marginRight: 10, 
     // alignItems: 'center',
     //justifyContent: 'center'
   },
